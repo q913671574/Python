0418d2a8d5bbe22b7a9be55cd93840d4a819a697
q913671574 <913671574@qq.com>
2020-10-04 16:24:34 +0800

    Update 美女吧_P61_060.py

--- 美女吧_P61_060.py
+++ 美女吧_P61_060.py
@@ -1,30 +1,31 @@
 import urllib.request
 import re
+from bs4 import BeautifulSoup
 
 def open_url(url):
     req = urllib.request.Request(url)
     req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3775.400 QQBrowser/10.6.4209.400')
     page = urllib.request.urlopen(req)
     html = page.read().decode('utf-8')
-
     return html
 
 
 def get_img(html):
-    p = r'<img src="http://[^"]+'
+    p = r'bpic=\"(.*?)\"'
     imglist = re.findall(p, str(html))
-    
+    picnum = 0
     for each in imglist:
         print(each)
+        if (".jpg" in each):
+            with open(str(picnum) + ".jpg", 'wb') as f:
+                img = urllib.request.urlopen(each, timeout = 3).read()
+                f.write(img)
+                picnum += 1
+                f.close()
 
     print('down')
     
-    """
-    for each in imglist:
-        filename = each.split("/")[-1] # 以/为分隔符， 取最后一项
-        urlib.request.urlretrieve(each, filename, None)
 
-    """
 
 
 if __name__ == '__main__':
